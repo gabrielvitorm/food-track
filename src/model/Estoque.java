1bde36973bf3557a983ef3ed59301f1dfb1e42a3
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2024-11-18 19:55:52 -0300

    Teste

--- src/model/Estoque.java
+++ src/model/Estoque.java
@@ -1,4 +1,39 @@
 package src.model;
 
-public class Estoque {
+public final class Estoque {
+
+    private int qtdProduto, qtdMinima;
+    private String produto;
+
+    public int getQtdProduto() {
+        return qtdProduto;
+    }
+
+    public void setQtdProduto(int qtdProduto) {
+        this.qtdProduto = qtdProduto;
+    }
+
+    public int getQtdMinima() {
+        return qtdMinima;
+    }
+
+    public void setQtdMinima(int qtdMinima) {
+        this.qtdMinima = qtdMinima;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public void setProduto(String produto) {
+        this.produto = produto;
+    }
+
+    public void atualizarQuantidade(){
+        qtdProduto -= qtdProduto;
+    }
+
+    public void verificarEstoqueMinimo(){
+
+    }
 }
