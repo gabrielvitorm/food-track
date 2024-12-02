af6fddac1f063d8bccdfd97e89836bfcb113f632
Pedro Queiroga <90732961+PedroQueiroga9@users.noreply.github.com>
2024-12-02 16:44:13 -0300

    Classe transacao correcao e implementacao

--- src/model/Transacao.java
+++ src/model/Transacao.java
@@ -5,4 +5,35 @@ import java.time.LocalDate;
 
 public abstract class Transacao implements Calculavel {
 
+
+    private int id;
+    private LocalDate data;
+    protected BigDecimal valorTotal;
+
+    // Construtor
+    public Transacao(int id, LocalDate data) {
+        this.id = id;
+        this.data = data;
+        this.valorTotal = BigDecimal.valueOf(0.0); // Inicializado como 0, calculado posteriormente.
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    // Método abstrato
+    public abstract void calcularValorTotal();
+
 }
+
+
+
